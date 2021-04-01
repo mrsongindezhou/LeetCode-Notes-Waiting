b3e87713313386a67cac53f84f8c7f110a192872
200637086@qq.com <200637086@qq.com>
2021-04-01 19:13:27 +0800

    2021-04-01

---
+++ 2021-04-01/leetcode-206.反转链表.go
@@ -0,0 +1,27 @@
+package _021_04_01
+
+/**
+ * @author     ：songdehua
+ * @emall      ：200637086@qq.com
+ * @date       ：Created in 2021/4/1 7:12 下午
+ * @description：
+ * @modified By：
+ * @version    ：$
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 递归实现
+func reverseList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	// last节点是链表对最后一个节点，通过每一层递归向上传递
+	last := reverseList(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return last
+}
